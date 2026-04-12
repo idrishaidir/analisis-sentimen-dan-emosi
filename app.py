fad16020484c215f84e345f93de35ede1978dcde
amier <amir81443@gmail.com>
2026-04-12 09:46:23 +0700

    membuat login dan register page, update app.py

--- app.py
+++ app.py
@@ -21,8 +21,8 @@ app.secret_key = "secret123"
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'ma9552905@gmail.com'
-app.config['MAIL_PASSWORD'] = 'qtbx bpnr keit mcyz'
+app.config['MAIL_USERNAME'] = 'email-lau-@gmail.com'
+app.config['MAIL_PASSWORD'] = 'mikir-kids'
 mail = Mail(app)
 
 # Inisialisasi Database
@@ -199,6 +199,7 @@ def analisis():
 
 
 @app.route("/result")
+@login_required
 def result():
     output_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "tweets-data", "output")
     
